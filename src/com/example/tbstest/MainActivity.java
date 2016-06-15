88bf5e82550b1567c7e82f4136040051a790dfd6
guangzhen <guangzhen.li@intel.com>
2016-06-15 10:44:51 +0800

    Init commit

---
+++ src/com/example/tbstest/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.tbstest;
+
+import com.tencent.smtt.sdk.CookieSyncManager;
+import com.tencent.smtt.sdk.WebSettings;
+import com.tencent.smtt.sdk.WebView;
+import com.tencent.smtt.sdk.WebSettings.LayoutAlgorithm;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+public class MainActivity extends Activity {
+	
+	private WebView mWebView;
+	private ViewGroup mViewParent;
+	private static final String mHomeUrl =  "file:///android_asset/glesjs/index_glsl.html";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+		try {
+			if (Integer.parseInt(android.os.Build.VERSION.SDK) >= 11) {
+				getWindow()
+						.setFlags(
+								android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+								android.view.WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+        setContentView(R.layout.activity_main);
+        
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+              | View.SYSTEM_UI_FLAG_FULLSCREEN
+              | View.SYSTEM_UI_FLAG_IMMERSIVE);
+        
+		mWebView = new WebView(this);
+		mViewParent = (ViewGroup) findViewById(R.id.webView1);
+		mViewParent.addView(mWebView, new FrameLayout.LayoutParams(
+				FrameLayout.LayoutParams.MATCH_PARENT,
+				FrameLayout.LayoutParams.MATCH_PARENT));
+		
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        WebView.setWebContentsDebuggingEnabled(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+
+		mWebView.loadUrl(mHomeUrl);
+		CookieSyncManager.createInstance(this);
+		CookieSyncManager.getInstance().sync();
+
+    }
+}
